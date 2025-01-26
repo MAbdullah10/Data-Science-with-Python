5099fed21f8059f47826b2aa272ee6220923a979
MAbdullah10 <muhammadabdullah100503@gmail.com>
2025-01-26 14:50:35 +0500

    Assignment 2 Statement

---
+++ 00 Assignments/Assignment 2/15 Assignment 2 Statement.py
@@ -0,0 +1,29 @@
+# Task 1: Tuple Practice
+# Create a tuple containing the names of 5 of your favorite fruits.
+# Print the second and fourth fruits from the tuple.
+# Try modifying one of the elements of the tuple (observe and note the error).
+# Convert the tuple into a list, add a new fruit to the list, and then convert it back into a tuple. Print the modified tuple.
+
+
+# Task 2: List Practice
+# Create a list of 8 random numbers.
+# Sort the list in ascending order and print it.
+# Add two new numbers to the list and print the updated list.
+# Remove the first occurrence of a specific number from the list.
+# Reverse the list and print it.
+
+
+# Task 3: Dictionary Practice
+# Create a dictionary where keys are student names and values are their scores in a test. (At least 5 entries).
+# Add a new student and their score to the dictionary.
+# Update the score of an existing student.
+# Remove one student from the dictionary.
+# Print all the keys and values separately.
+
+
+# Task 4: Set Practice
+# Create a set of 6 unique colors.
+# Add two new colors to the set.
+# Try adding a duplicate color and observe what happens.
+# Remove a specific color from the set.
+# Find the union and intersection of this set with another set of 4 colors.
